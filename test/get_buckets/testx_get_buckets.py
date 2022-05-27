f445457fd407fe915eb6e3d9c84f1202bd850cff
LucasFDutra <lucasfelipedutra@gmail.com>
2022-05-27 20:41:15 -0300

    [CHANGE] exclui arquivos que não são mais necessário
    > test/get_buckets/testx_get_buckets.py
    > test/get_buckets/xtest_base.py

--- test/get_buckets/testx_get_buckets.py
+++
@@ -1,9 +0,0 @@
-from main import app
-
-def test_if_all_buckets_will_be_return():
-    """Verificar se todos os buckets criados serão retornados sem passar nenhuma restrição do que mostrar ou não"""
-    client = app.test_client()
-    res = client.get('/api/get_buckets')
-
-    assert res.json == ["mybucket-1", "mybucket-2"]
-
