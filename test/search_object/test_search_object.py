8b051fd464d048d78a4d41f814c336965c974b72
LucasFDutra <lucasfelipedutra@gmail.com>
2022-05-27 20:39:46 -0300

    [CHANGE] melhorando os testes
    > test/search_object/test_search_object.py

--- test/search_object/test_search_object.py
+++ test/search_object/test_search_object.py
@@ -1,32 +1,44 @@
 from src.routes import search_object
+import test.search_object as test_init
 
+def convert_dict(objects):
+    return [
+        {
+            'key': obj['Key'],
+            'last_modified': obj['LastModified'].strftime('%Y-%m-%d'),
+            'name': obj['Key'].split('/')[-1],
+            'size': obj['Size']
+        } 
+        for obj in objects
+    ]
 
-def test_if_filters_the_200_abc_files(pre_test):
+def test_if_filters_the_abc_abc_x_files(pre_test):
     res = search_object.search_object(
-        bucket = 'abc',
+        bucket = 'xpto',
         prefix = 'abc',
         search_term = 'abc',
         next_continuation_token = False
     )
     
-    assert len(res.get('objects')) == 200
+    assert res.get('objects') == convert_dict(test_init.object_with_abc_abc_x)
 
-def test_if_filters_the_400_2022_files(pre_test):
+def test_if_filters_the_abc_xyz_x_files(pre_test):
     res = search_object.search_object(
-        bucket = 'abc',
+        bucket = 'xpto',
         prefix = 'abc',
-        search_term = '2022',
+        search_term = 'xyz',
         next_continuation_token = False
     )
     
-    assert len(res.get('objects')) == 400
+    assert res.get('objects') == convert_dict(test_init.object_with_abc_xyz_x)
 
-def test_if_filters_any_files(pre_test):
+
+def test_if_filters_all_files_that_ends_with_txt(pre_test):
     res = search_object.search_object(
         bucket = 'abc',
-        prefix = 'abc',
-        search_term = 'zzz',
+        prefix = '',
+        search_term = '.txt',
         next_continuation_token = False
     )
-    
-    assert len(res.get('objects')) == 0
+    expected = test_init.object_with_abc_abc_x + test_init.object_with_abc_xyz_x + test_init.object_with_xyz_abc_x + test_init.object_with_xyz_xyz_x
+    assert len(res.get('objects')) == len(expected)
